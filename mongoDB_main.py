1fc2687eb7d9ba25be5ea9ea2df72b3b3c438bc6
ghuioio <54397393+ghuioio@users.noreply.github.com>
2023-05-04 23:08:38 +0700

    0405

--- mongoDB_main.py
+++ mongoDB_main.py
@@ -4,7 +4,7 @@ import csv
 # Connect to MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["crypto_db"]
-tweets_collection = db["crypto_tweets"]
+tweets_collection = db["crypto_tweets_0305"]
 
 def update_document(unique_id, new_data):
     tweets_collection.update_one(
@@ -13,7 +13,7 @@ def update_document(unique_id, new_data):
     )
 
 # Import CSV data into MongoDB
-with open('100coin100tweets.csv', 'r', encoding='utf-8') as csvfile:
+with open('100coins100_0305tweets.csv', 'r', encoding='utf-8') as csvfile:
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader:
         tweets_collection.insert_one(row)
